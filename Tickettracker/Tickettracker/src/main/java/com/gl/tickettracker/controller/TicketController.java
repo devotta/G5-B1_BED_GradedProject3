7e40d4374d93c6be7610046601cad8402d7fc0f2
Antony Roy <47647718+devotta@users.noreply.github.com>
2023-04-24 18:28:57 +0500

    Add files via upload

--- Tickettracker/Tickettracker/src/main/java/com/gl/tickettracker/controller/TicketController.java
+++ Tickettracker/Tickettracker/src/main/java/com/gl/tickettracker/controller/TicketController.java
@@ -1,7 +1,6 @@
 package com.gl.tickettracker.controller;
 
 import java.util.List;
-import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
